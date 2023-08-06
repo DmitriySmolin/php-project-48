3991c91da449bd78b85e543bb073dbc70dc8ccab
neox56 <neox56@gmail.com>
2023-08-06 18:46:44 +0400

    finish step 2

---
+++ src/Differ.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Differ;
+
+use Exception;
+use function cli\line;
+use function Funct\Collection\pluck;
+
+function printDiff(string $first, string $second, string $format = 'stylish'): void
+{
+    $firstArray = json_decode(file_get_contents($first), true);
+    $secondArray = json_decode(file_get_contents($second), true);
+
+    $oldSign = '-';
+    $newSign = '+';
+    $sameSign = ' ';
+    $differences = genDiff($firstArray, $secondArray);
+
+    ksort($differences);
+
+    line('{');
+
+    foreach ($differences as $property => $change) {
+        $status = $change['type'];
+
+        match ($status) {
+            'added' => line(" %s %s: %s", $newSign, $property, convertValueToString($change['actual'])),
+            'deleted' => line(" %s %s: %s", $oldSign, $property, convertValueToString($change['old'])),
+            'changed' => [
+                line(" %s %s: %s", $oldSign, $property, convertValueToString($change['old'])),
+                line(" %s %s: %s", $newSign, $property, convertValueToString($change['actual']))
+            ],
+            'same' => line(" %s %s: %s", $sameSign, $property, convertValueToString($change['actual'])),
+            default => throw new Exception('Invalid diff status'),
+        };
+
+    }
+
+    line('}');
+
+}
+
+function convertValueToString($value): string
+{
+    return match (true) {
+        $value === true => 'true',
+        $value === false => 'false',
+        default => $value
+    };
+}
+
+function genDiff(array $first, array $second): array
+{
+    $merged = [...$first, ...$second];
+
+    $plucked = [];
+
+    foreach ($merged as $key => $value) {
+        $plucked[$key] = pluck([$merged, $first, $second], $key);
+    }
+
+    $mapped = [];
+    foreach ($plucked as $key => $value) {
+        [, $first, $second] = $value;
+
+        $comparisonState = match (true) {
+            is_null($first) && !is_null($second) => 'added',
+            !is_null($first) && is_null($second) => 'deleted',
+            $first === $second => 'same',
+            default => 'changed',
+        };
+
+        $mapped[$key] = [
+            'old' => $first,
+            'actual' => $second,
+            'type' => $comparisonState
+        ];
+    }
+
+    return $mapped;
+}
